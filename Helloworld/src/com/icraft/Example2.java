2bda948be62411cc68cb5872c0cab1b8bc987c69
Robertorres9 <103595212+Robertorres9@users.noreply.github.com>
2022-04-15 10:49:19 -0400

    Example2 class commit from eclipse

---
+++ Helloworld/src/com/icraft/Example2.java
@@ -0,0 +1,17 @@
+package com.icraft;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		
+		
+		
+		
+	
+	}
+
+}
